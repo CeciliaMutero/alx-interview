2bca409092f0b42bc7e041c478eb93993eb52b7b
Cecilia <cecilmutero66@gmail.com>
2024-10-17 22:21:14 +0300

    0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,32 +1,44 @@
 #!/usr/bin/python3
+"""
+This module defines a function that simulates a prime
+number game between Maria and Ben.
+"""
+
 def isWinner(x, nums):
-    if x == 0 or not nums:
+    """
+    Determines the winner
+    of the prime number game.
+    """
+    if x <= 0 or not nums:
         return None
-    
-    max_n = max(nums)
-    
-    sieve = [True] * (max_n + 1)
-    sieve[0] = sieve[1] = False
-    for i in range(2, int(max_n ** 0.5) + 1):
-        if sieve[i]:
-            for multiple in range(i * i, max_n + 1, i):
-                sieve[multiple] = False
-    
-    prime_moves = [0] * (max_n + 1)
-    
-    for i in range(2, max_n + 1):
-        prime_moves[i] = prime_moves[i - 1]
-        if sieve[i]:
-            prime_moves[i] += 1
-    
+
+    def sieve_of_eratosthenes(n):
+        """Find all prime numbers up to n using the
+        Sieve of Eratosthenes."""
+        sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
+        for i in range(2, int(n**0.5) + 1):
+            if sieve[i]:
+                for j in range(i * i, n + 1, i):
+                    sieve[j] = False
+        return [i for i in range(2, n + 1) if sieve[i]]
+
+    max_num = max(nums)
+    primes_up_to_max = sieve_of_eratosthenes(max_num)
+
+    prime_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if i in primes_up_to_max else 0)
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
-        if prime_moves[n] % 2 == 0:
-            ben_wins += 1
-        else:
+        if prime_count[n] % 2 == 1:
             maria_wins += 1
+        else:
+            ben_wins += 1
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
